0564e5556b3ecc3fb7b91130e483befed2292081
Yidne <yamiyekn@gmail.com>
2023-05-09 18:02:19 +0300

    Add species column to the animals table.

--- schema.sql
+++ schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE animals (
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
-    weight_kg decimal(5,2)
+    weight_kg decimal(5,2),
+    species varchar(200)
 );
