7317d7cc2a89f19e5f9ece91a03a34db5f19f7d4
Yidne <yamiyekn@gmail.com>
2023-05-11 18:13:29 +0300

    Create vets, specialization and visits tables and modify the schema.sql file.

--- schema.sql
+++ schema.sql
@@ -16,7 +16,6 @@ id BIGSERIAL NOT NULL PRIMARY KEY,
 full_name varchar(100) NOT NULL,
 age INTEGER 
 );
-
 CREATE TABLE species (
 id BIGSERIAL NOT NULL PRIMARY KEY,
 name varchar(100) NOT NULL 
@@ -25,3 +24,24 @@ name varchar(100) NOT NULL
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id BIGINT REFERENCES species (id);
 ALTER TABLE animals ADD COLUMN owner_id BIGINT REFERENCES owners (id);
+
+
+--Create a table named vets
+CREATE TABLE vets (
+id BIGSERIAL NOT NULL PRIMARY KEY,
+name varchar(100),
+age INT,
+date_of_graduation DATE );
+
+--Create a "join table" called specializations
+CREATE TABLE specializations (
+	species_id INT,
+	vet_id INT
+);
+
+--Create a "join table" called visits
+CREATE TABLE visits (
+    animal_id INT,
+    vet_id INT,
+    date_of_visit DATE
+);
